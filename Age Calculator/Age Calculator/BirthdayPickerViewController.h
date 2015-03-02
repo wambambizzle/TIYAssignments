7007645a14b92b4f517387a60a5d16a8cad52106
Jordan Anderson <mr.janderson14@gmail.com>
2015-03-02 17:02:36 -0500

    App almost complete, fix age calculation method's logic

---
+++ Age Calculator/Age Calculator/BirthdayPickerViewController.h
@@ -0,0 +1,18 @@
+//
+//  BirthdayPickerViewController.h
+//  Age Calculator
+//
+//  Created by Jordan Anderson on 3/2/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+#import "UIKit/UIKit.h"
+#import "ViewController.h"
+
+
+@interface BirthdayPickerViewController : UIViewController
+
+@property (strong, nonatomic) id<ViewController> delegate;
+
+
+
+@end
