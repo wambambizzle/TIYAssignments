8e74a491d920b007403f59293e856029e700fcfa
Jordan Anderson <mr.janderson14@gmail.com>
2015-03-03 08:42:26 -0500

    05 -- Age Calculator storyboard and coding tasks complete

---
+++ 05 -- Age Calculator/Age Calculator/ViewController.h
@@ -0,0 +1,21 @@
+//
+//  ViewController.h
+//  Age Calculator
+//
+//  Created by Jordan Anderson on 3/2/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol ViewControllerBdayPickerDelegate
+
+- (void) birthdayDateWasChoosen:(NSDate *) birthdayDate;
+
+@end
+
+@interface ViewController : UIViewController <ViewControllerBdayPickerDelegate>
+
+
+@end
+
---
+++ 05 -- Age Calculator/Age Calculator/ViewController.m
@@ -0,0 +1,137 @@
+//
+//  ViewController.m
+//  Age Calculator
+//
+//  Created by Jordan Anderson on 3/2/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "BirthdayPickerViewController.h"
+
+
+@interface ViewController ()
+{
+    
+ NSDateFormatter *formatDate;
+ NSDate *usersBirthDate;
+    
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *currentDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *usersBirthdayLabel;
+@property (weak, nonatomic) IBOutlet UILabel *usersAgeLabel;
+
+
+
+- (IBAction)calculateAge:(UIButton *)sender;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"Age Calculator";
+    
+
+    formatDate = [[NSDateFormatter alloc] init];
+
+    
+    NSString *formatString = [NSDateFormatter dateFormatFromTemplate:@"MMM dd yyyy"
+                                                             options:0
+                                                              locale:[NSLocale currentLocale]];
+    
+    
+    [formatDate setDateFormat:formatString];
+    
+    self.currentDateLabel.text = [formatDate stringFromDate:[NSDate date]];
+    self.usersAgeLabel.text = @"--";
+    self.usersBirthdayLabel.text = @"Please select your Birthdate";
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Navigation
+
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    
+    if ([segue.identifier isEqualToString:@"ShowBirthdateDatePickerSegue"])
+    {
+        BirthdayPickerViewController *bdayPickerVC = (BirthdayPickerViewController *)[segue destinationViewController];
+        
+        bdayPickerVC.delegate = self; // more infoooooo !
+    }
+    
+}
+
+
+#pragma mark - ViewControllerDelegate
+
+- (void)birthdayDateWasChoosen:(NSDate *)birthdayDate
+{
+    self.usersBirthdayLabel.text = [formatDate stringFromDate:birthdayDate];
+    usersBirthDate = birthdayDate;
+}
+
+#pragma mark - Action Handlers
+
+- (void)findAge
+{
+    
+    NSDateComponents *todayComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitMonth|NSCalendarUnitDay|NSCalendarUnitYear fromDate: [NSDate date]];
+    
+    NSDateComponents *birthdayComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitMonth|NSCalendarUnitDay|NSCalendarUnitYear fromDate:usersBirthDate];
+    
+    NSInteger age = todayComponents.year - birthdayComponents.year;
+    
+    if(todayComponents.month < birthdayComponents.month)
+    {
+        age = age - 1;
+    }
+    else if (todayComponents.month == birthdayComponents.month)
+    {
+        if (todayComponents.day < birthdayComponents.day) {
+            age = age -1;
+        }
+    }
+    
+    self.usersAgeLabel.text = [NSString stringWithFormat:@"%ld", (long)age];
+    
+}
+
+
+
+- (IBAction)calculateAge:(UIButton *)sender
+{
+    
+    [self findAge];
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@end
