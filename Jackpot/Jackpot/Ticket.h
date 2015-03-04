76eb881a28c0c19d14c10b223ffc3b1b7979c5d7
Jordan Anderson <mr.janderson14@gmail.com>
2015-03-04 13:45:39 -0500

    Refactor Code, more efficent

---
+++ Jackpot/Jackpot/Ticket.h
@@ -0,0 +1,15 @@
+//
+//  Ticket.h
+//  Jackpot
+//
+//  Created by Jordan Anderson on 3/3/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Ticket : NSObject
+
++ (instancetype)ticketUsingQuickPick;
+
+@end
---
+++ Jackpot/Jackpot/TicketsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TicketsTableViewController.h
+//  Jackpot
+//
+//  Created by Jordan Anderson on 3/3/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TicketsTableViewController : UITableViewController
+
+@end
