28064df20ccc30b212db9bb5a9f0b25adbc55ced
Jordan Anderson <mr.janderson14@gmail.com>
2015-03-10 14:35:58 -0400

    Percent convesion and ChangeSign logic and implementation complete

---
+++ CalculatorBrain/CalculatorBrain/CalculatorBrain.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.h
+//  CalculatorBrain
+//
+//  Created by Jordan Anderson on 3/10/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+@end
---
+++ CalculatorBrain/CalculatorBrain/CalculatorBrain.m
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.m
+//  CalculatorBrain
+//
+//  Created by Jordan Anderson on 3/10/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+@end
