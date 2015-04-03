556f81c44fda4c7af8a38f3d1e070560ed004604
Jordan Anderson <mr.janderson14@gmail.com>
2015-04-03 13:36:45 -0400

    blah

---
+++ VenueMenue/VenueMenue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VenueMenue
+//
+//  Created by Jordan Anderson on 4/2/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
